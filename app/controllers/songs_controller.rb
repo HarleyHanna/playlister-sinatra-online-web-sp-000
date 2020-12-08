5711e9b31f7e73f3e5a47492ce114492e943bf7c
Harley Hanna <harleyhannawrit@gmail.com>
2020-12-08 20:11:20 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,45 @@
+require 'rack-flash'
+
+class SongsController < ApplicationController
+  use Rack::Flash
+
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+
+  post '/songs' do
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genre]
+    @song.save
+    flash[:message] = "Successfully created song."
+    redirect to("/songs/#{@song.slug}")
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch 'songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:slug])
+    @song.artist = Artist.find_or_create_by(params[:artist][:name])
+    @song.genred_ids = params[:genres]
+    @song.save
+    flash[:message] = "Successfully updated Song."
+    redirect to "/songs/#{@song.slug}"
+  end
+
+end
